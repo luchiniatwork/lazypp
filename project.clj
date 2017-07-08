9387e8efa8f3084a4ea34e81b61d08700ef7c361
Tiago Luchini <luchini@work.co>
2017-07-07 20:30:08 -0400

    Getting ready to publish

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lazypp/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject lazypp/lein-template "0.1.0"
+  :description "The PowerPoint/Keynote of the Lazy Pragmatic Presenter"
+  :url "https://github.com/luchiniatwork/lazypp"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true)
